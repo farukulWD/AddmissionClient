3cf8e7a41c98f9547b96eb5fcc22135223b8e71e
Farukul Islam <farukgb1999@gmail.com>
2023-07-22 00:34:29 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-5xl">
+        This collage admission side
+      </h1>
+    </>
+  );
+}
+
+export default App;
